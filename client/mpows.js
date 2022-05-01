18d59566e1d049b6f9d249410df2cc726de22a73
Gene <gmlyth@gmail.com>
2022-05-01 18:12:27 -0500

    Corrected js file so it calls the function on load.

--- client/mpows.js
+++ client/mpows.js
@@ -1,6 +1,8 @@
 //If you're recording page loads, replace this with the url for the
 //Lambda function for page loads, after the stack has been created.
 const LAMBDA_FUNCTION_URL_PAGE_LOADS = ""; 
+//If you want to capture any cookies, put them in this string array.
+const COOKIES = {};
 
 function recordPageLoad()
 {
@@ -17,51 +19,9 @@ function recordPageLoad()
     }
 }
 
-// Dean Edwards/Matthias Miller/John Resig
-function init() {
-    // quit if this function has already been called
-    if (arguments.callee.done) return;
-  
-    // flag this function so we don't do the same thing twice
-    arguments.callee.done = true;
-  
-    // kill the timer
-    if (_timer) clearInterval(_timer);
-  
-    //Uncomment below if recording page loads.
-    //recordPageLoad();
-  };
-  
-  /* for Mozilla/Opera9 */
-  if (document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", init, false);
-  }
-  
-  /* for Internet Explorer */
-  /*@cc_on @*/
-  /*@if (@_win32)
-    document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
-    var script = document.getElementById("__ie_onload");
-    script.onreadystatechange = function() {
-      if (this.readyState == "complete") {
-        init(); // call the onload handler
-      }
-    };
-  /*@end @*/
-  
-  /* for Safari */
-  if (/WebKit/i.test(navigator.userAgent)) { // sniff
-    var _timer = setInterval(function() {
-      if (/loaded|complete/.test(document.readyState)) {
-        init(); // call the onload handler
-      }
-    }, 10);
-  }
-  
-  /* for other browsers */
-  window.onload = init;
+recordPageLoad();
 
-  function getCookie(cname) {
+function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
@@ -76,3 +36,47 @@ function init() {
     }
     return "";
   }
+
+// // Dean Edwards/Matthias Miller/John Resig
+// function init() {
+//     // quit if this function has already been called
+//     if (arguments.callee.done) return;
+  
+//     // flag this function so we don't do the same thing twice
+//     arguments.callee.done = true;
+  
+//     // kill the timer
+//     if (_timer) clearInterval(_timer);
+  
+//     //Uncomment below if recording page loads.
+//     //recordPageLoad();
+//   };
+  
+//   /* for Mozilla/Opera9 */
+//   if (document.addEventListener) {
+//     document.addEventListener("DOMContentLoaded", init, false);
+//   }
+  
+//   /* for Internet Explorer */
+//   /*@cc_on @*/
+//   /*@if (@_win32)
+//     document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
+//     var script = document.getElementById("__ie_onload");
+//     script.onreadystatechange = function() {
+//       if (this.readyState == "complete") {
+//         init(); // call the onload handler
+//       }
+//     };
+//   /*@end @*/
+  
+//   /* for Safari */
+//   if (/WebKit/i.test(navigator.userAgent)) { // sniff
+//     var _timer = setInterval(function() {
+//       if (/loaded|complete/.test(document.readyState)) {
+//         init(); // call the onload handler
+//       }
+//     }, 10);
+//   }
+  
+//   /* for other browsers */
+//   window.onload = init;
