d32fbd363e4e62ed46b354c8ed2f33ab3df77cd4
Gene <gmlyth@gmail.com>
2022-05-03 08:53:02 -0500

    Initial research into capturing page clicks.

--- client/mpows.js
+++ client/mpows.js
@@ -37,46 +37,27 @@ function getCookie(cname) {
     return "";
   }
 
-// // Dean Edwards/Matthias Miller/John Resig
-// function init() {
-//     // quit if this function has already been called
-//     if (arguments.callee.done) return;
-  
-//     // flag this function so we don't do the same thing twice
-//     arguments.callee.done = true;
-  
-//     // kill the timer
-//     if (_timer) clearInterval(_timer);
-  
-//     //Uncomment below if recording page loads.
-//     //recordPageLoad();
-//   };
-  
-//   /* for Mozilla/Opera9 */
-//   if (document.addEventListener) {
-//     document.addEventListener("DOMContentLoaded", init, false);
-//   }
-  
-//   /* for Internet Explorer */
-//   /*@cc_on @*/
-//   /*@if (@_win32)
-//     document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
-//     var script = document.getElementById("__ie_onload");
-//     script.onreadystatechange = function() {
-//       if (this.readyState == "complete") {
-//         init(); // call the onload handler
-//       }
-//     };
-//   /*@end @*/
-  
-//   /* for Safari */
-//   if (/WebKit/i.test(navigator.userAgent)) { // sniff
-//     var _timer = setInterval(function() {
-//       if (/loaded|complete/.test(document.readyState)) {
-//         init(); // call the onload handler
-//       }
-//     }, 10);
-//   }
-  
-//   /* for other browsers */
-//   window.onload = init;
+var addEvent = function (obj, evType, fn){
+    if (obj.addEventListener){
+            obj.addEventListener(evType, fn, false);
+            return true;
+    }else if (obj.attachEvent){
+            var r = obj.attachEvent("on"+evType, fn);
+            return r;
+    } else {
+            return false;
+    }
+};
+
+addEvent(window,'load',function () {
+    var links = Array.prototype.slice.call(
+        document.getElementsByTagName('a')
+    );
+    
+    var count = links.length;
+    for(var i = 0; i < count; i++) {
+        links[i].addEventListener('click', function(e) {
+            alert(`ID: of link: ${e.currentTarget.id} href of link: ${e.currentTarget.href} title of link: ${e.currentTarget.title} class of link: ${e.currentTarget.className} innerHTML of link: ${e.currentTarget.innerHTML}`);
+        });
+    }
+ });
